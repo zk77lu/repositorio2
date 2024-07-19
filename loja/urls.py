eb113db55a0e9d9c3f226f572073df0851f26b31
zk77lu <zikardozo@gmail.com>
2024-07-19 11:34:50 -0300

    atualizado

--- loja/urls.py
+++ loja/urls.py
@@ -17,82 +17,82 @@ import ipaddress
 #    /________|  |     \ /              \  |         \ |__________/    \__________/
 
 
-# def calcular_data_12_dias_uteis():
-#     dias_uteis = 0
-#     data_atual = datetime.now()
-#     data_futura = data_atual
+def calcular_data_12_dias_uteis():
+    dias_uteis = 0
+    data_atual = datetime.now()
+    data_futura = data_atual
     
-#     while dias_uteis < 12:
+    while dias_uteis < 12:
         
-#         data_futura += timedelta(days=1)
+        data_futura += timedelta(days=1)
         
         
-#         if data_futura.weekday() < 5:  
-#             dias_uteis += 1
+        if data_futura.weekday() < 5:  
+            dias_uteis += 1
     
     
-#     return data_futura
+    return data_futura
 
-# def formatar_data_por_extenso(data):
+def formatar_data_por_extenso(data):
     
-#     meses = {
-#         1: 'janeiro', 2: 'fevereiro', 3: 'março', 4: 'abril', 
-#         5: 'maio', 6: 'junho', 7: 'julho', 8: 'agosto', 
-#         9: 'setembro', 10: 'outubro', 11: 'novembro', 12: 'dezembro'
-#     }
-    
-    
-#     dia = data.day
-#     mes = meses[data.month]
-#     data_formatada = f'{dia} de {mes}'
-    
-#     return data_formatada
-
-
+    meses = {
+        1: 'janeiro', 2: 'fevereiro', 3: 'março', 4: 'abril', 
+        5: 'maio', 6: 'junho', 7: 'julho', 8: 'agosto', 
+        9: 'setembro', 10: 'outubro', 11: 'novembro', 12: 'dezembro'
+    }
     
     
+    dia = data.day
+    mes = meses[data.month]
+    data_formatada = f'{dia} de {mes}'
+    
+    return data_formatada
+
+
+    
+    
 
 
 
 
 
 
-# def obter_endereco_ipv6_local():
-#     try:
+def obter_endereco_ipv6_local():
+    try:
         
-#         s = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
-#         s.connect(("ipv6.google.com", 80))  
-#         endereco_ipv6 = s.getsockname()[0]
-#         s.close()
-#         return endereco_ipv6
-#     except socket.error:
-#         return None
+        s = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
+        s.connect(("ipv6.google.com", 80))  
+        endereco_ipv6 = s.getsockname()[0]
+        s.close()
+        return endereco_ipv6
+    except socket.error:
+        return None
 
-# def get_location(request):
+def get_location(request):
 
-#     ip_address = obter_endereco_ipv6_local()
+    ip_address = obter_endereco_ipv6_local()
 
-#     try:
-#         url = f"https://ipinfo.io/{ip_address}/json"
-#         response = requests.get(url)
-#         response.raise_for_status()
-#         data = response.json()
+    try:
+        url = f"https://ipinfo.io/{ip_address}/json"
+        response = requests.get(url)
+        response.raise_for_status()
+        data = response.json()
 
-#         context = {
-#             'data': data
-#         }
+        context = {
+            'data': data
+        }
        
         
 
-#         return context
+        return context
 
-#     except requests.exceptions.RequestException as e:
+    except requests.exceptions.RequestException as e:
     
-#         context = {
-#             'data': None
-#         }
+        context = {
+            'data': None
+        }
         
-#         return context
+        return context
 
 
 
@@ -101,188 +101,188 @@ import ipaddress
 
 
 
-# # #######################3333333###################
+# #######################3333333###################
 
 
-# def buscar_endereco_por_cep(cep):
-#     url = f'https://viacep.com.br/ws/{cep}/json/'
-#     response = requests.get(url)
-#     if response.status_code == 200:
-#         return response.json()
-#     else:
-#         return None
+def buscar_endereco_por_cep(cep):
+    url = f'https://viacep.com.br/ws/{cep}/json/'
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None
 
 
-# def carregar_produtos():
-#     with open('infos.json', 'r', encoding='utf-8') as file:
-#         produtos = json.load(file)
+def carregar_produtos():
+    with open('infos.json', 'r', encoding='utf-8') as file:
+        produtos = json.load(file)
         
-#         for produto in produtos:
-#             produto['id'] = int(produto['id'])
-#     return produtos
+        for produto in produtos:
+            produto['id'] = int(produto['id'])
+    return produtos
 
-# produtos = carregar_produtos()
-
-
+produtos = carregar_produtos()
 
 
-# def produto_detalhes(request, produto_id=None):
+
+
+def produto_detalhes(request, produto_id=None):
     
-#     data_12_dias_uteis = calcular_data_12_dias_uteis()
+    data_12_dias_uteis = calcular_data_12_dias_uteis()
     
     
-#     data_formatada = formatar_data_por_extenso(data_12_dias_uteis)
+    data_formatada = formatar_data_por_extenso(data_12_dias_uteis)
     
 
-#     diaMes = {
-#         'data_formatada': data_formatada,
-#     }
-#     ipv6 = get_location(request)
+    diaMes = {
+        'data_formatada': data_formatada,
+    }
+    ipv6 = get_location(request)
 
-#     if request.method == 'POST' and request.headers.get('x-requested-with') == 'XMLHttpRequest':
+    if request.method == 'POST' and request.headers.get('x-requested-with') == 'XMLHttpRequest':
         
-#         cep = request.POST.get('cep_input')
-#         endereco = buscar_endereco_por_cep(cep)
-#         if endereco:
+        cep = request.POST.get('cep_input')
+        endereco = buscar_endereco_por_cep(cep)
+        if endereco:
             
-#             return JsonResponse({'localidade': endereco.get('localidade'), 'uf': endereco.get('uf')})
-#         else:
-#             return JsonResponse({'error': 'Endereço não encontrado'}, status=404)
-#     else:
+            return JsonResponse({'localidade': endereco.get('localidade'), 'uf': endereco.get('uf')})
+        else:
+            return JsonResponse({'error': 'Endereço não encontrado'}, status=404)
+    else:
        
         
-#         if produto_id is not None:
-#             produto = next((p for p in produtos if p['id'] == produto_id), None)
-#             if produto is None:
-#                 return render(request, '404.html')  
-#             return render(request, 'product.html', {'ipv6':ipv6,'diaMes':diaMes,'produto': produto, 'todos_os_produtos': json.dumps(produtos)})
-#         else:
+        if produto_id is not None:
+            produto = next((p for p in produtos if p['id'] == produto_id), None)
+            if produto is None:
+                return render(request, '404.html')  
+            return render(request, 'product.html', {'ipv6':ipv6,'diaMes':diaMes,'produto': produto, 'todos_os_produtos': json.dumps(produtos)})
+        else:
             
-#             return render(request, 'product.html', {'ipv6':ipv6,'diaMes':diaMes,'produto': None, 'todos_os_produtos': json.dumps(produtos)})
+            return render(request, 'product.html', {'ipv6':ipv6,'diaMes':diaMes,'produto': None, 'todos_os_produtos': json.dumps(produtos)})
         
-# def pag_index(request,produto_id=None):
+def pag_index(request,produto_id=None):
      
-#     data_12_dias_uteis = calcular_data_12_dias_uteis()
+    data_12_dias_uteis = calcular_data_12_dias_uteis()
     
     
-#     data_formatada = formatar_data_por_extenso(data_12_dias_uteis)
+    data_formatada = formatar_data_por_extenso(data_12_dias_uteis)
     
     
-#     diaMes = {
-#         'data_formatada': data_formatada,
-#     }
-#     ipv6 = get_location(request)
+    diaMes = {
+        'data_formatada': data_formatada,
+    }
+    ipv6 = get_location(request)
 
-#     if request.method == 'POST' and request.headers.get('x-requested-with') == 'XMLHttpRequest':
+    if request.method == 'POST' and request.headers.get('x-requested-with') == 'XMLHttpRequest':
         
-#         cep = request.POST.get('cep_input')
-#         endereco = buscar_endereco_por_cep(cep)
-#         if endereco:
+        cep = request.POST.get('cep_input')
+        endereco = buscar_endereco_por_cep(cep)
+        if endereco:
             
-#             return JsonResponse({'localidade': endereco.get('localidade'), 'uf': endereco.get('uf')})
-#         else:
-#             return JsonResponse({'error': 'Endereço não encontrado'}, status=404)
-#     else:
+            return JsonResponse({'localidade': endereco.get('localidade'), 'uf': endereco.get('uf')})
+        else:
+            return JsonResponse({'error': 'Endereço não encontrado'}, status=404)
+    else:
        
         
-#         if produto_id is not None:
-#             produto = next((p for p in produtos if p['id'] == produto_id), None)
-#             if produto is None:
-#                 return render(request, '404.html')  
-#             return render(request, 'index.html', {'ipv6':ipv6,'diaMes':diaMes,'produto': produto, 'todos_os_produtos': json.dumps(produtos)})
-#         else:
+        if produto_id is not None:
+            produto = next((p for p in produtos if p['id'] == produto_id), None)
+            if produto is None:
+                return render(request, '404.html')  
+            return render(request, 'index.html', {'ipv6':ipv6,'diaMes':diaMes,'produto': produto, 'todos_os_produtos': json.dumps(produtos)})
+        else:
             
-#             return render(request, 'index.html', {'ipv6':ipv6,'diaMes':diaMes,'produto': None, 'todos_os_produtos': json.dumps(produtos)})
+            return render(request, 'index.html', {'ipv6':ipv6,'diaMes':diaMes,'produto': None, 'todos_os_produtos': json.dumps(produtos)})
     
 
        
-# def pag_contato(request,produto_id=None):
+def pag_contato(request,produto_id=None):
      
-#     data_12_dias_uteis = calcular_data_12_dias_uteis()
+    data_12_dias_uteis = calcular_data_12_dias_uteis()
     
     
-#     data_formatada = formatar_data_por_extenso(data_12_dias_uteis)
+    data_formatada = formatar_data_por_extenso(data_12_dias_uteis)
     
     
-#     diaMes = {
-#         'data_formatada': data_formatada,
-#     }
+    diaMes = {
+        'data_formatada': data_formatada,
+    }
    
 
-#     if request.method == 'POST' and request.headers.get('x-requested-with') == 'XMLHttpRequest':
+    if request.method == 'POST' and request.headers.get('x-requested-with') == 'XMLHttpRequest':
         
         
-#         cep = request.POST.get('cep_input')
-#         endereco = buscar_endereco_por_cep(cep)
-#         if endereco:
+        cep = request.POST.get('cep_input')
+        endereco = buscar_endereco_por_cep(cep)
+        if endereco:
             
-#             return JsonResponse({'localidade': endereco.get('localidade'), 'uf': endereco.get('uf')})
-#         else:
-#             return JsonResponse({'error': 'Endereço não encontrado'}, status=404)
-#     else:
+            return JsonResponse({'localidade': endereco.get('localidade'), 'uf': endereco.get('uf')})
+        else:
+            return JsonResponse({'error': 'Endereço não encontrado'}, status=404)
+    else:
         
         
        
         
-#         if produto_id is not None:
-#             produto = next((p for p in produtos if p['id'] == produto_id), None)
-#             if produto is None:
+        if produto_id is not None:
+            produto = next((p for p in produtos if p['id'] == produto_id), None)
+            if produto is None:
                 
-#                 return render(request, '404.html')  
-#             return render(request, 'contact.html', {'diaMes':diaMes,'produto': produto, 'todos_os_produtos': json.dumps(produtos)})
-#         else:
+                return render(request, '404.html')  
+            return render(request, 'contact.html', {'diaMes':diaMes,'produto': produto, 'todos_os_produtos': json.dumps(produtos)})
+        else:
             
             
-#             return render(request, 'contact.html', {'diaMes':diaMes,'produto': None, 'todos_os_produtos': json.dumps(produtos)})
+            return render(request, 'contact.html', {'diaMes':diaMes,'produto': None, 'todos_os_produtos': json.dumps(produtos)})
     
       
-# def pag_sacola(request,produto_id=None):
+def pag_sacola(request,produto_id=None):
      
-#     data_12_dias_uteis = calcular_data_12_dias_uteis()
+    data_12_dias_uteis = calcular_data_12_dias_uteis()
     
     
-#     data_formatada = formatar_data_por_extenso(data_12_dias_uteis)
+    data_formatada = formatar_data_por_extenso(data_12_dias_uteis)
     
     
-#     diaMes = {
-#         'data_formatada': data_formatada,
-#     }
+    diaMes = {
+        'data_formatada': data_formatada,
+    }
    
 
-#     if request.method == 'POST' and request.headers.get('x-requested-with') == 'XMLHttpRequest':
+    if request.method == 'POST' and request.headers.get('x-requested-with') == 'XMLHttpRequest':
         
         
-#         cep = request.POST.get('cep_input')
-#         endereco = buscar_endereco_por_cep(cep)
-#         if endereco:
+        cep = request.POST.get('cep_input')
+        endereco = buscar_endereco_por_cep(cep)
+        if endereco:
             
-#             return JsonResponse({'localidade': endereco.get('localidade'), 'uf': endereco.get('uf')})
-#         else:
-#             return JsonResponse({'error': 'Endereço não encontrado'}, status=404)
-#     else:
+            return JsonResponse({'localidade': endereco.get('localidade'), 'uf': endereco.get('uf')})
+        else:
+            return JsonResponse({'error': 'Endereço não encontrado'}, status=404)
+    else:
         
         
        
         
-#         if produto_id is not None:
-#             produto = next((p for p in produtos if p['id'] == produto_id), None)
-#             if produto is None:
+        if produto_id is not None:
+            produto = next((p for p in produtos if p['id'] == produto_id), None)
+            if produto is None:
                 
-#                 return render(request, '404.html')  
-#             return render(request, 'sacola.html', {'diaMes':diaMes,'produto': produto, 'todos_os_produtos': json.dumps(produtos)})
-#         else:
+                return render(request, '404.html')  
+            return render(request, 'sacola.html', {'diaMes':diaMes,'produto': produto, 'todos_os_produtos': json.dumps(produtos)})
+        else:
             
             
-#             return render(request, 'sacola.html', {'diaMes':diaMes,'produto': None, 'todos_os_produtos': json.dumps(produtos)})
+            return render(request, 'sacola.html', {'diaMes':diaMes,'produto': None, 'todos_os_produtos': json.dumps(produtos)})
     
-# def sobre_loja(request):
-#     return render(request,'sob_loja.html')
+def sobre_loja(request):
+    return render(request,'sob_loja.html')
 
-# def poli_privac(request):
-#     return render(request,'polipri.html')
+def poli_privac(request):
+    return render(request,'polipri.html')
 
-# def poli_entreg(request):
-#     return render(request,'polientre.html')
+def poli_entreg(request):
+    return render(request,'polientre.html')
 
 def troc_devolu(request):
     return render(request,'trodevo.html')
@@ -293,13 +293,13 @@ def troc_devolu(request):
 
 
 urlpatterns = [
-    # path('produto/<int:produto_id>/', produto_detalhes, name='produto_detalhes'),
-    # path( '', pag_index ,name='pag_index'),
-    # path('sacola/', pag_sacola ,name='sacola'),
-    # path('contato/', pag_contato ,name='contato'),
-    # path('sobre/',sobre_loja ,name='sobre_loja' ),
-    # path('privacidade/',poli_privac ,name='poli_privac' ),
-    # path('entrega/',poli_entreg ,name='poli_entreg'),
+    path('produto/<int:produto_id>/', produto_detalhes, name='produto_detalhes'),
+    path( '', pag_index ,name='pag_index'),
+    path('sacola/', pag_sacola ,name='sacola'),
+    path('contato/', pag_contato ,name='contato'),
+    path('sobre/',sobre_loja ,name='sobre_loja' ),
+    path('privacidade/',poli_privac ,name='poli_privac' ),
+    path('entrega/',poli_entreg ,name='poli_entreg'),
     path('devolucao/',troc_devolu ,name='troc_devolu')
 ]
 
